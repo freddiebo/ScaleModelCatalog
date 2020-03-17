083d91f4f1cbb81b59f021e4447e58f4e5e0a3cd
VBondarev <freddiebo@yandex.ru>
2020-03-17 14:42:35 +0300

    Initial Commit

---
+++ ScaleModelCatalog/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ScaleModelCatalog
+//
+//  Created by  Vladislav Bondarev on 17.03.2020.
+//  Copyright © 2020 Vladislav Bondarev. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
